3679dec1f51fc7dcf837be47acc4d936b820db3f
DrXripper <99840084+DrXripper@users.noreply.github.com>
2022-07-20 14:31:45 +0530

    Add files via upload

--- js/admin-analicts.js
+++ js/admin-analicts.js
@@ -1,33 +1,66 @@
 
 const addOrderTerminalFilters = ( filters ) => {
 
-    return [
-        {
+    var changefilters = [];
+
+    if(wcSettings.orderTerminals){
+         changefilters.push({
             label: 'Terminal',
             staticParams: [],
             param: 'terminal',
             showFilters: () => true,
             defaultValue: '0',
             filters: [ ...( wcSettings.orderTerminals || [] ) ],
-        },
-        {
+        });
+    }
+
+    if(wcSettings.orderSource){
+         changefilters.push({
             label: 'Source',
             staticParams: [],
             param: 'source',
             showFilters: () => true,
             defaultValue: '0',
             filters: [ ...( wcSettings.orderSource || [] ) ],
-        },
-        {
+        });
+    }
+
+    if(wcSettings.orderStore){
+         changefilters.push({
             label: 'Store',
             staticParams: [],
             param: 'store',
             showFilters: () => true,
             defaultValue: '0',
             filters: [ ...( wcSettings.orderStore || [] ) ],
-        },
-        ...filters,
-    ];
+        });
+    }
+
+    if(wcSettings.paymentMethods){
+         changefilters.push({
+            label: 'Payment Methods',
+            staticParams: [],
+            param: 'paymentMethods',
+            showFilters: () => true,
+            defaultValue: '0',
+            filters: [ ...( wcSettings.paymentMethods || [] ) ],
+        });
+    }
+
+    if(wcSettings.orderStatus){
+         changefilters.push({
+            label: 'Order Status',
+            staticParams: [],
+            param: 'orderStatus',
+            showFilters: () => true,
+            defaultValue: '0',
+            filters: [ ...( wcSettings.orderStatus || [] ) ],
+        });
+    }
+
+    changefilters.push( ...filters );     
+
+    return changefilters;
 };
  
 window.wp.hooks.addFilter( 'woocommerce_admin_orders_report_filters', 'dev-blog-example', addOrderTerminalFilters );
